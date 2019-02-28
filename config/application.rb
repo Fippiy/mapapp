00b5a5d68a0b09f185abaa44d77c9e7f10cedc7c
Fippiy <profier4@gmail.com>
2019-02-28 12:52:07 +0900

    rewrite application_controller settings

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,15 @@ module Mapapp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
+
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+    end
   end
 end

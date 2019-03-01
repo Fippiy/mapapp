0cdb99f7e739578d8280393ac8db1286ddd09b61
Fippiy <profier4@gmail.com>
2019-03-01 15:44:12 +0900

    Setting session controller

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,27 +1,40 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def new
+    super
+  end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    super
+  end
 
   # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  def destroy
+    super
+  end
 
-  # protected
+  def failed
+    redirect_to root_path
+  end
+
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  def configure_sign_in_params
+    # devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+    devise_parameter_sanitizer.permit(:sign_in) do |params|
+      params.permit(:username, :password, :password_confirmation)
+    end
+  end
+
+  def auth_options
+    # 失敗時に recall に設定したパスのアクションが呼び出されるので変更
+    # { scope: resource_name, recall: "#{controller_path}#new" } # デフォルト
+    { scope: resource_name, recall: "#{controller_path}#failed" }
+  end
 end

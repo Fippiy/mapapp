beaeebbb2d9eff5f07100fcca335e0c8be0fab3e
Fippiy <profier4@gmail.com>
2019-03-01 21:18:37 +0900

    Setting user search route

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,9 @@ Rails.application.routes.draw do
   root 'routes#index'
   resources :maps, only: [:index]
   resources :routes, only: [:index]
+  resources :users do
+    collection do
+      get "search"
+    end
+  end
 end

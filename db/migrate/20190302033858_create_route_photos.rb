ee857db1f8445ba2786a71fd924c3ca531710bb3
Fippiy <profier4@gmail.com>
2019-03-02 12:40:41 +0900

    Edit migration route_photos

--- db/migrate/20190302033858_create_route_photos.rb
+++ db/migrate/20190302033858_create_route_photos.rb
@@ -1,7 +1,8 @@
 class CreateRoutePhotos < ActiveRecord::Migration[5.2]
   def change
     create_table :route_photos do |t|
-
+      t.text :photo
+      t.references :route, foreign_key: true
       t.timestamps
     end
   end

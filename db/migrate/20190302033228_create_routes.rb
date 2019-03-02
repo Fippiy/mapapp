42221fde556cc9721e5100a048396e2defaea531
Fippiy <profier4@gmail.com>
2019-03-02 12:38:12 +0900

    Edit migration routes

--- db/migrate/20190302033228_create_routes.rb
+++ db/migrate/20190302033228_create_routes.rb
@@ -1,7 +1,9 @@
 class CreateRoutes < ActiveRecord::Migration[5.2]
   def change
     create_table :routes do |t|
-
+      t.references :user, foreign_key: true
+      t.string :title,null: false
+      t.text :description
       t.timestamps
     end
   end

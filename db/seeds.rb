f086f498ae56546d56019c569e9e1dad88522869
Fippiy <profier4@gmail.com>
2019-03-02 13:58:05 +0900

    Setting sample seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#create first sample user
+User.create(
+  username: "t",
+  email: "test@test.com",
+  password: "111111"
+)
+
+#create user sample
+20.times do
+  User.create(
+    username: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password
+  )
+end
+1.upto(20) do |index|
+  Route.create(
+    user_id: index,
+    title: Faker::Coffee.blend_name,
+    description: Faker::Coffee.notes,
+    route_photos_attributes: [
+      {
+        photo: Faker::Avatar.image,
+        route_id: index
+      }
+    ]
+  )
+end
